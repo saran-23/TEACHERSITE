73a1de4e13b82110963363ed22fdcd156a47a728
saran-23 <50368466+saran-23@users.noreply.github.com>
2020-08-20 19:39:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const port = 8080;
+const app = express();
+
+app.use(express.static('public'));
+
+app.use('/homepage', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'html', 'homepage.html'));
+});
+
+app.use('/question-bank', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'html', 'question-paper.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Server started http://localhost:${port}/homepage`);
+});
+
+//  Home, Syllabus, Qb, Materials, QA, Practicals, Video Lectures
